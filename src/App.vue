751063c300576ccc6414668bc8a4389e3c02817d
paulaami <pmilkowska@onet.pl>
2022-05-08 17:44:30 +0200

    added files

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <user-data @add-input-values="setUserData"></user-data>
+  <active-user :username="user.name" :userage="user.age"></active-user>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: "Paula",
+        age: 31,
+      },
+    };
+  },
+  methods: {
+    setUserData(name, age) {
+      this.user = {
+        name: name,
+        age: +age,
+      };
+    },
+  },
+};
+</script>
---
+++ src/components/UserData.vue
@@ -0,0 +1,22 @@
+<template>
+  <input type="text" placeholder="your name" v-model="enteredUsername" />
+  <input type="number" placeholder="your age" v-model="enteredUserage" />
+  <button @click="submitInput">Set User Data</button>
+</template>
+
+<script>
+export default {
+  emits: ["add-input-values"],
+  data() {
+    return {
+      enteredUsername: "",
+      enteredUserage: "",
+    };
+  },
+  methods: {
+    submitInput() {
+      this.$emit("add-input-values", this.enteredUsername, this.enteredUserage);
+    },
+  },
+};
+</script>
